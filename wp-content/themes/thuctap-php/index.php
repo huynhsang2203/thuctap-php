24da2fad315c154f17782801916e83cde7a20371
huynhsang2203 <huynhsang220300@gmail.com>
2022-02-19 20:50:11 +0700

    a

--- wp-content/themes/thuctap-php/index.php
+++ wp-content/themes/thuctap-php/index.php
@@ -6,225 +6,53 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Thành công</title>
-    <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory')?>accset/css/thanh-toan-thanh-cong.css">
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory') ?>/accset/css/thanh-toan-thanh-cong.css">
 </head>
 
 <body>
-    <div class="bg-out ">
-        <div class="bg-in ">
-            <div class="bg-vector ">
-                <img src="image/bg.svg " alt="vector ">
-            </div>
-            <h1>
-                Thanh toán
-            </h1>
-        </div>
-
+    <div class="bg">
+        <!-- HEADER -->
         <header>
-            <div class="banner ">
-                <img src="image/header.svg " alt=" ">
-            </div>
-            <div class="LittleLogo ">
-                <img src="image/little.png " alt="littlelogo ">
-            </div>
-            <div class="tags ">
-                <ul class="tags-nav ">
-                    <li id="tags-trangchu ">
-                        <a href="index.html ">
-                            <p id="text-trangchu ">Trang chủ</p>
-                        </a>
+            <img src="<?php bloginfo('stylesheet_directory') ?>/image/header.svg" alt="header">
+            <img id="little_little_logo" src="<?php bloginfo('stylesheet_directory') ?>/image/little.png" alt="little-logo">
 
-                    </li>
-                    <li id="tags-sukien ">
-                        <a href="sukien.html ">
-                            <p id="text-sukien ">Sự kiện</p>
-                        </a>
+            <div class="nav-tags">
 
-                    </li>
-                    <li id="tags-lienhe ">
-                        <a href="lienHe.html ">
-                            <p id="text-lienhe ">Liên hệ</p>
-                        </a>
-                    </li>
-                </ul>
-                <ul class="tags-phone ">
-                    <li id="tags-phone-li ">
-                        <img src="image/icon-phone.svg " alt="phone ">
-                        <p>
-                            0123456789
-                        </p>
-                    </li>
-                </ul>
+                <div class="nav-tags tags-phone">
+                    <p id="text-phone">
+                        0123456789
+                    </p>
+                    <div class="nav-tags tags-phone phone-svg">
+                        <img src="<?php bloginfo('stylesheet_directory') ?>/image/icon-phone.svg" alt="icon-phone">
+                    </div>
+                </div>
+
+
+                <div class="nav-tags tags-3">
+                    <ul>
+                        <li><a href=""></a><p>Trang chủ</p></li>
+                        <li><a href=""></a>Sự kiện</li>
+                        <li><a href=""></a>Liên hệ</li>
+                    </ul>
+                    <!-- <p>
+                        xin chào
+                    </p> -->
+                </div>
+
             </div>
         </header>
-
-        <!-- Khung bự chứa nội dung chính -->
-        <div class="container ">
-            <div class="svg-container-1 ">
-                <img src="svg-thanhtoan/container-1.svg " alt=" ">
-            </div>
-            <div class="svg-container-2 ">
-                <img src="svg-thanhtoan/container-2.svg " alt=" ">
-            </div>
-            <div class="svg-container-3 ">
-                <img src="svg-thanhtoan/container-3.svg " alt=" ">
-            </div>
-            <div class="content-1 ">
-                <p>
-                    Số tiền thanh toán
-                </p>
-                <div class="content-1-1 ">
-                    <p>
-                        <?= $result->cost ?> vnđ
-                    </p>
-                </div>
-            </div>
-            <div class="content-2 ">
-                <p>
-                    Số lượng vé
-                </p>
-                <div class="content-2-1 ">
-                    <div class="content-2-1-1 ">
-                        <p>
-                            <?= $result->tickets ?>
-                        </p>
-                    </div>
-                    <p>
-                        Vé
-                    </p>
-                </div>
-            </div>
-            <div class="content-3 ">
-                <p>
-                    Ngày sử dụng
-                </p>
-                <div class="content-3-1 ">
-                    <p>
-                        <?= $result->date_start ?>
-                    </p>
-                </div>
-
-            </div>
-
-            <!-- Thông tin liên hệ -->
-            <div class="content-4 ">
-                <p>
-                    Thông tin liên hệ
-                </p>
-                <div class="content-4-1 ">
-                    <p>
-                        <?= $result->email ?>
-                    </p>
-                </div>
-            </div>
-
-            <!-- Điện thoại-->
-            <div class="content-5 ">
-                <p>
-                    Điện thoại
-                </p>
-                <div class="content-5-1 ">
-                    <p>
-                        <?= $result->phone ?>
-                    </p>
-                </div>
-            </div>
-
-            <!-- Email-->
-            <div class="content-6 ">
-                <p>
-                    Email
-                </p>
-                <div class="content-6-1 ">
-                    <h4>
-                        <?= $result->email ?>
-                    </h4>
-                </div>
-            </div>
-
-            <!-- Main Right -->
-            <div class="content-right ">
-
-                <!-- Số thẻ-->
-                <div class="content-7 ">
-                    <p>
-                        Số thẻ
-                    </p>
-                    <div class="content-7-1 ">
-                        <p>
-                            <?= $result->card_number ?>
-                        </p>
-                    </div>
-                </div>
-
-                <!-- Họ tên chủ thẻ-->
-                <div class="content-8 ">
-                    <p>
-                        Họ tên chủ thẻ
-                    </p>
-                    <div class="content-8-1 ">
-                        <p>
-                            <?= $result->card_name ?>
-                        </p>
-                    </div>
-                </div>
-
-                <!-- Ngày hết hạn -->
-                <div class="content-9 ">
-                    <p>
-                        Ngày hết hạn
-                    </p>
-                    <div class="content-9-1 ">
-                        <input type="date " name="date " id="date ">
-                    </div>
-                </div>
-
-                <!-- CVV/CVC-->
-                <div class="content-10 ">
-                    <p>
-                        CVV/CVC
-                    </p>
-
-                    <input type="password " class="content-10-1 " value="<?=$result->cvv_cvc ?>">
-
-                </div>
+        <!-- END_HEADER -->
 
 
-            </div>
-
-            <div class="button-pay">
-                <input type="submit" value="Thanh toán">
-                <div class="button-bg">
-                    <img src="svg-thanhtoan/btn-pay.svg" alt="btn-pay">
-                </div>
-            </div>
+        <!-- MAIN -->
+        <div class="main" style="background-color: red;width: 100px;height: 200px;top: 200px; position: relative;left: 100px;">
 
 
         </div>
+        <!-- END_MAIN -->
 
-        <!-- Khung chứa chữ Vé Cổng Vé Gia Đình -->
-        <div class="container-1">
-            <p>
-                VÉ CỔNG - VÉ GIA ĐÌNH
-            </p>
-            <img src="svg-thanhtoan/container-4.svg" alt="">
-
-        </div>
-
-        <!-- Khung chứa chữ Thông Tin Thanh Toán -->
-        <div class="container-2">
-            <img src="svg-thanhtoan/container-4.svg" alt="">
-            <p>
-                THÔNG TIN THANH TOÁN
-            </p>
-        </div>
 
     </div>
-
-    <?php
-    wp_footer();
-    ?>
-
 </body>
 
 </html>
