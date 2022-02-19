9f47126ea4cd0419fb8e2c8884a3758ecc4e0627
huynhsang2203 <huynhsang220300@gmail.com>
2022-02-20 01:30:25 +0700

    a

--- wp-content/themes/thuctap-php/index.php
+++ wp-content/themes/thuctap-php/index.php
@@ -19,14 +19,132 @@
                 <img src="<?php bloginfo('stylesheet_directory') ?>/image/bg.svg" alt="">
             </div>
 
+            <!-- Khung chứa vé -->
+            <div class="cnt-tickets">
+                <img src="<?php bloginfo('stylesheet_directory') ?>/svg-thanh-toan-thanh-cong/ctn-tickets.svg" alt="">
+
+                <!-- Vé 1 -->
+                <div class="ticket-1">
+                    <div class="qr">
+                        <img src="<?php bloginfo('stylesheet_directory') ?>/image/qr.png" alt="">
+
+                    </div>
+
+                    <p id="qr-code">
+                        ALT20210501
+                    </p>
+
+                    <p id="ticket-type">
+                        VÉ CỔNG
+                    </p>
+
+                    <p id="---">
+                        ---
+                    </p>
+
+                    <p id="ticket-date">
+                        Ngày sử dụng: 31/05/2021
+                    </p>
+
+                    <div class="svg-tick">
+                        <img src="<?php bloginfo('stylesheet_directory') ?>/svg-thanh-toan-thanh-cong/tick-1.svg" alt="">
+                    </div>
+
+                </div>
+
+                <!-- Vé 2 -->
+                <div class="ticket-2">
+                    <div class="qr">
+                        <img src="<?php bloginfo('stylesheet_directory') ?>/image/qr.png" alt="">
+
+                    </div>
+
+                    <p id="qr-code">
+                        ALT20210501
+                    </p>
+
+                    <p id="ticket-type">
+                        VÉ CỔNG
+                    </p>
+
+                    <p id="---">
+                        ---
+                    </p>
+
+                    <p id="ticket-date">
+                        Ngày sử dụng: 31/05/2021
+                    </p>
+
+                    <div class="svg-tick">
+                        <img src="<?php bloginfo('stylesheet_directory') ?>/svg-thanh-toan-thanh-cong/tick-1.svg" alt="">
+                    </div>
+
+                </div>
+
+                <!-- Vé 3 -->
+                <div class="ticket-3">
+                    <div class="qr">
+                        <img src="<?php bloginfo('stylesheet_directory') ?>/image/qr.png" alt="">
+
+                    </div>
+
+                    <p id="qr-code">
+                        ALT20210501
+                    </p>
+
+                    <p id="ticket-type">
+                        VÉ CỔNG
+                    </p>
+
+                    <p id="---">
+                        ---
+                    </p>
+
+                    <p id="ticket-date">
+                        Ngày sử dụng: 31/05/2021
+                    </p>
+
+                    <div class="svg-tick">
+                        <img src="<?php bloginfo('stylesheet_directory') ?>/svg-thanh-toan-thanh-cong/tick-1.svg" alt="">
+                    </div>
+
+                </div>
+
+                <!-- Vé 4 -->
+                <div class="ticket-4">
+                    <div class="qr">
+                        <img src="<?php bloginfo('stylesheet_directory') ?>/image/qr.png" alt="">
+                    </div>
+
+                    <p id="qr-code">
+                        ALT20210501
+                    </p>
+
+                    <p id="ticket-type">
+                        VÉ CỔNG
+                    </p>
+
+                    <p id="---">
+                        ---
+                    </p>
+
+                    <p id="ticket-date">
+                        Ngày sử dụng: 31/05/2021
+                    </p>
+
+                    <div class="svg-tick">
+                        <img src="<?php bloginfo('stylesheet_directory') ?>/svg-thanh-toan-thanh-cong/tick-1.svg" alt="">
+                    </div>
+
+                </div>
+
+            </div>
+
             <!-- Ảnh nhân vật Alvin_Arnold_Votay1 -->
             <div class="img_Alvin_Arnold_Votay1">
                 <img src="<?php bloginfo('stylesheet_directory') ?>/image/Alvin_Arnold_Votay1.png" alt="">
             </div>
 
-            <div class="cnt-tickets">
-
-            </div>
         </div>
         <!-- END_MAIN -->
 
